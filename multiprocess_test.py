b1d16fdbf8394318f2d4eaff1e3e68265a4e48c1
Javier <wudunliang@gmail.com>
2019-01-24 14:38:33 +0800

    multiprocess communication test over queue

--- multiprocess_test.py
+++ multiprocess_test.py
@@ -1,4 +1,4 @@
-import multiprocessing, select, sys
+import multiprocessing, select, sys, os
 
 def write(s):
     print(f'before {hex(id(s))}')
@@ -55,7 +55,48 @@ def pipetest():
         for wr in writeable:
             if sendCache:
                 wr.send(sendCache.pop())
-    
+
+#----------------------------------------------------------------------
+def writequeue(q):
+    receiveCache = []
+    running = True
+    while running:
+        readable, writeable, exeptions = select.select([q._reader],[q._writer],[])
+        for read in readable:
+            data = q.get()
+            print(f'data {data}, addr {hex(id(data))}, pid {os.getpid()}')
+            receiveCache.append(data)
+            if data == b'\n':
+                running = False
+        for wr in writeable:
+            if receiveCache:
+                q.put(receiveCache.pop()+b' '+str(os.getpid()).encode('utf-8'))
+
+def queuetest():
+    queue = multiprocessing.Queue()
+    p1 = multiprocessing.Process(target = writequeue, args = (queue,))
+    p2 = multiprocessing.Process(target = writequeue, args = (queue,))    
+    p1.start()
+    p2.start()
+    running = True
+    sendCache = []
+    while running:
+        readable, writeable, exceptions = select.select([queue._reader, sys.stdin], [queue._writer], [])
+        for read in readable:
+            if read == queue._reader:
+                data = queue.get()
+                print(f'data back {data}, addr {hex(id(data))}, pid {os.getpid()}')
+                if data == b'\n':
+                    running = False
+            elif read == sys.stdin:
+                data = read.readline().encode('utf-8')
+                print(f'stdin data {data}, addr {hex(id(data))}')
+                sendCache.append(data)
+        for wr in writeable:
+            if sendCache:
+                queue.put(sendCache.pop())    
+
 if __name__ == "__main__":
     #directtest()
-    pipetest()
+    #pipetest()
+    queuetest()
