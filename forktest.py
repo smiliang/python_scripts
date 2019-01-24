a7a9c3ab7fdf83b02fbe9dcab446894fd8a9184d
Javier <wudunliang@gmail.com>
2019-01-24 12:02:29 +0800

    fork test about virtual memory

--- forktest.py
+++ forktest.py
@@ -9,8 +9,8 @@ list = []
 a = random.randint(0,10000)
 print(f'random {a}')
 list.append(a)
-print(f'list addr : {hex(id(list))}')
-print(f'a addr ; {hex(id(a))}')
+print(f'list origin addr : {hex(id(list))}')
+print(f'a addr : {hex(id(a))}')
 
 if True:
     child = []
@@ -23,18 +23,24 @@ if True:
             # We are in the child process.
             print(f"{os.getpid()} (child) just was created by {os.getppid()}. i {i}")
             isc = True
-            print(f'list addr : {hex(id(list))}')
-            print(f'a addr ; {hex(id(a))}')            
+            print(f'list child addr : {hex(id(list))}')
+            print(f'a addr : {hex(id(a))}')
+            a += 5
+            print(f'a modified addr : {hex(id(a))}')
             break
         else:
             # We are in the parent process.
             print(f"{os.getpid()} (parent) just created {pid}. i {i}")
             child.append(pid)
     if not isc:
-        print(f"after childs {child}")
+        print(f"before childs {child}")
         print(f'servers {list}')
-        print(f'list addr : {hex(id(list))}')
-        print(f'a addr ; {hex(id(a))}')        
+        print(f'list father addr : {hex(id(list))}')
+        print(f'a addr : {hex(id(a))}')
+        print(f'a value : {a}')
         for c in child:
             p,s = os.waitpid(c, 0)
             print(f'{p} status : {s}')
+        print(f'list after father addr : {hex(id(list))}')            
+        print(f'a addr : {hex(id(a))}')
+        print(f'a value : {a}')
